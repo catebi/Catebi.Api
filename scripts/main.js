75302b0b9d1be2f26ed03d81a229d07589761283
ks <zejafo@gmail.com>
2023-08-20 23:05:49 +0400

    catebi simple site 0.01

---
+++ scripts/main.js
@@ -0,0 +1,99 @@
+const translations = {
+    'en': {
+        'title': "Welcome to Catebi’s website!",
+        'section1Title': "Have you seen some cats missing the tip of one ear?",
+        'section1Content': "The reason they are missing the tip of one ear is that it indicates that they have been surgically neutered. So far, hundreds of cats have been spayed in Tbilisi and we intend to do more.",
+        'section2Title': "Who are we?",
+        'section2Content': "Catebi is a group of volunteers in Tbilisi. Thanks to us, more than 200 cats have been sterilized in Tbilisi, and we continue to do so.",
+        'section3Title': "Why neuter the cats?",
+        'section3Content': "Firstly, neutering helps control the animal population. Remember: in 6 years, a pair of cats and their offspring can produce up to 420,000 kittens.",
+        'section4Title': "Want to help?",
+        'section4List': [
+            "Help financially. The cost of sterilizing one cat is 50₾.",
+            "Help with your hands. You can catch cats.",
+            "Auto help. If you have a car, you can assist with transportation.",
+            "Donate. Offer unnecessary items for the animals.",
+            "Photo session for cats. Sometimes captured cats remain in temporary housing.",
+            "Tell your friends. Share information about Catebi."
+        ],
+        'section5Title': "Neuter your pet cat?",
+        'section5Content': 'Email us at <a href="mailto:catebi.tbilisi@gmail.com">catebi.tbilisi@gmail.com</a> to find out where you can get your cat neutered for a reasonable price.'
+    },
+    'ge': {
+        'title': "",
+        'section1Title': "გინახავთ კატა, რომელსაც ერთი ყურის წვერი აკლია?",
+        'section1Content': "კატებს ერთი ყურის წვერი აკლიათ, რადგან ეს იმაზე მეტყველებს, რომ ისინი ქირურგიულად იქნა სტერილური. მოხალისეთა თვითორგანიზებული ჯგუფი აკავებდა კატებს, ასტერირებდა და შემდეგ ათავისუფლებდა მათი მკურნალობის შემდეგ. ეს არის ჩვეულებრივი პრაქტიკა, რომელიც წარმატებით გამოიყენება მრავალ სხვადასხვა ქვეყანაში უსახლკარო კატების რაოდენობის შესამცირებლად. როგორც ვიცით, ჩვენი ჯგუფი არის ყველაზე მასშტაბური ოპერაცია თბილისში, რომელმაც ასეთი დავალება შეასრულა. თბილისში ამ დრომდე ასობით კატაა სტერილური და მეტის გაკეთებას ვაპირებთ.",
+        'section2Title': "Ვინ ვართ ჩვენ?",
+        'section2Content': "Catebi არის მოხალისეთა ჯგუფი თბილისში. ჩვენ ვეხმარებით კატებს, ზოგიერთ ძაღლს და ძალიან იშვიათად სხვა ცხოველებს. ჩვენი ამოცანაა ქალაქში რაც შეიძლება მეტი კატის სტერილიზაცია და ამაზე საზოგადოების ყურადღების მიქცევა. ჩვენი წყალობით თბილისში 200-ზე მეტი კატა გასტერილდა და ვაგრძელებთ.",
+        'section3Title': "რატომ სჭირდებათ კატების სტერილიზაცია?",
+        'section3Content': "ჯერ ერთი, სტერილიზაცია არის ცხოველთა პოპულაციის კონტროლის ერთ-ერთი გზა, რაც ნიშნავს, რომ ამცირებს ქუჩაში უსახლკარო ცხოველების რაოდენობას. მეორეც, ის ახანგრძლივებს სიცოცხლეს ჰორმონზე დამოკიდებული დაავადებების მნიშვნელოვანი შემცირების გამო. მესამე, ის საშუალებას გაძლევთ მშვიდად დაიძინოთ თქვენ და თქვენს მეზობლებს, რადგან ეს დაგეხმარებათ შეამციროთ ღამის შეჯვარების თამაშების რაოდენობა თქვენს სახლთან ახლოს. დაიმახსოვრეთ: 6 წელიწადში წყვილმა კატამ და მისმა შთამომავლობამ შეიძლება 420 000-მდე კნუტი გააჩინოს.",
+        'section4Title': "გსურთ დაეხმაროთ უსახლკარო ცხოველების სტერილიზაციაში?",
+        'section4List': [
+            "დაეხმარეთ ფინანსურად. ერთი კატის სტერილიზაციის ღირებულებაა 50 ლარი.",
+            "დაეხმარეთ ხელებით. თქვენ შეგიძლიათ დაიჭიროთ კატები ახლომდებარე ეზოებიდან და.",
+            "ავტომატური დახმარება. თუ გყავთ მანქანა, დაგვეხმარებით დატყვევებული კატების დროებით.",
+            "ყიდვა/გაცემა. თქვენ შეგიძლიათ შეუწიროთ ცხოველებისთვის არასაჭირო ნივთები.",
+            "კატების ფოტოსესია. ხანდახან დატყვევებულ კატებს ჯანმრთელობის მიზეზების გამო არ ასტერილებენ.",
+            "უთხარი მეგობრებს. გააზიარეთ ინფორმაცია Catebi-ის."
+        ],
+        'section5Title': "როგორ შემიძლია ჩემი შინაური ცხოველის კატა სტერილიზაცია?",
+        'section5Content': 'გთხოვთ მოგვწეროთ მისამართზე catebi.tbilisi@gmail.com, რომ მიიღოთ ინფორმაცია იმის შესახებ, თუ სად შეგიძლიათ თქვენი კატის სტერილიზაცია გონივრულ ფასად.'
+    },
+    'ru': {
+        'title': "Добро пожаловать на сайт Catebi!",
+        'section1Title': "Вам попадались кошки с обрезанным кончиком одного уха?",
+        'section1Content': "Причина, по которой у них обрезан кончик одного уха, заключается в том, что это указывает на то, что их хирургически стерилизовали. До сих пор в Тбилиси было стерилизовано сотни кошек, и мы намерены продолжить эту работу.",
+        'section2Title': "Кто мы?",
+        'section2Content': "Catebi - это группа волонтеров в Тбилиси. Благодаря нам было стерилизовано более 200 кошек в Тбилиси, и мы продолжаем это делать.",
+        'section3Title': "Почему стерилизовать кошек?",
+        'section3Content': "Во-первых, стерилизация помогает контролировать популяцию животных. Помните: за 6 лет пара кошек и их потомство могут произвести до 420 000 котят.",
+        'section4Title': "Хотите помочь?",
+        'section4List': [
+            "Помощь финансово. Стоимость стерилизации одной кошки составляет 50₾",
+            "Помощь руками. Вы можете ловить кошек.",
+            "Помощь на автомобиле. Если у вас есть автомобиль, вы можете помочь с транспортировкой.",
+            "Пожертвуйте. Предложите ненужные вещи для животных.",
+            "Фотосессия для кошек. Иногда пойманные кошки остаются во временном жилье.",
+            "Расскажите своим друзьям. Поделитесь информацией о Catebi."
+        ],
+        'section5Title': "Хотите стерилизовать свою домашнюю кошку?",
+        'section5Content': 'Напишите нам на <a href="mailto:catebi.tbilisi@gmail.com">catebi.tbilisi@gmail.com</a>, чтобы узнать, где вы можете стерилизовать свою кошку по разумной цене.'
+    }
+};
+
+const changeLanguage = (lang) => {
+    for (let key in translations[lang]) {
+        if (key.endsWith("List")) {
+            const listElement = document.querySelector(`[data-translate-key=${key}]`);
+            listElement.innerHTML = "";  // Clear the current list items
+            translations[lang][key].forEach(item => {
+                const li = document.createElement('li');
+                li.innerHTML = item;
+                listElement.appendChild(li);
+            });
+        } else {
+            const element = document.querySelector(`[data-translate-key=${key}]`);
+            element.innerHTML = translations[lang][key];
+        }
+    }
+}
+
+const updateActiveLanguage = (lang) => {
+    document.querySelectorAll('.lang').forEach(link => {
+        link.classList.remove('active'); // Remove active class from all language links
+    });
+    document.querySelector(`[data-lang=${lang}]`).classList.add('active'); // Add active class to selected language link
+}
+
+// Default language
+changeLanguage('en');
+updateActiveLanguage('en');
+
+// Event delegation for language switch
+document.querySelector("#languageLinks").addEventListener('click', function(e) {
+    if (e.target.classList.contains('lang')) {
+        const selectedLang = e.target.getAttribute('data-lang');
+        changeLanguage(selectedLang);
+        updateActiveLanguage(selectedLang);
+    }
+});
