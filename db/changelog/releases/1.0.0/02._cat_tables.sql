7f2beec83c5bba600f6096bcb185729f41cefd2c
tk0nik <72498532+tk0nik@users.noreply.github.com>
2023-09-30 17:53:38 +0300

    Update 02._cat_tables.sql

--- db/changelog/releases/1.0.0/02._cat_tables.sql
+++ db/changelog/releases/1.0.0/02._cat_tables.sql
@@ -8,7 +8,6 @@ drop table if exists ctb.cat_sex;
 drop table if exists ctb.volunteer;
 drop table if exists ctb.color;
 
-
 -- cat table
 create table ctb.cat (
     cat_id serial PRIMARY KEY,
@@ -19,14 +18,14 @@ create table ctb.cat (
     created_time timestamp NOT NULL default (now() at time zone 'utc'),
     last_edited_time timestamp NOT NULL default (now() at time zone 'utc'),
     notion_page_url text,
-	cat_sex_id integer NOT NULL,
-	cat_collar_id integer,
-	cat_house_space_id integer,
-	in_date date default NOW()::date,	--can be null or '' in notion
-	out_date date,
-	neutered_date date,
-	responsible_volunteer_id integer, --notion name is 'deliverer'
-	comment text
+    cat_sex_id integer NOT NULL,
+    cat_collar_id integer,
+    cat_house_space_id integer,
+    in_date date default NOW()::date,    --can be null or '' in notion
+    out_date date,
+    neutered_date date,
+    responsible_volunteer_id integer, --notion name is 'deliverer'
+    comment text
 );
 
 comment on table ctb.cat is 'Кошка/кот';
@@ -73,8 +72,8 @@ comment on column ctb.cat_image_url.type is 'Тип картинки';
 create table ctb.color (
     color_id serial PRIMARY KEY,
     name text NOT NULL UNIQUE,
-	rgb_code varchar (15) NOT NULL UNIQUE, 
-	hex_code varchar (7) NOT NULL UNIQUE
+    rgb_code varchar (15) NOT NULL UNIQUE, 
+    hex_code varchar (7) NOT NULL UNIQUE
 );
 
 comment on table ctb.color is 'Словарь цветов (для ошейников, отметок и проч)';
@@ -85,10 +84,10 @@ comment on column ctb.color.hex_code is 'Запись в формате "#000000
 
 -- cat_sex table
 create table ctb.cat_sex (
-    cat_sex_id serial PRIMARY KEY,	
-    name text NOT NULL UNIQUE,	
+    cat_sex_id serial PRIMARY KEY,    
+    name text NOT NULL UNIQUE,    
     color_id integer,
-	
+    
     FOREIGN KEY (color_id) references ctb.color(color_id)
 );
 
@@ -99,10 +98,10 @@ comment on column ctb.cat_sex.color_id is 'ID цвета';
 
 -- cat_collar table
 create table ctb.cat_collar (
-    cat_collar_id serial PRIMARY KEY,	
-    name text NOT NULL UNIQUE,	
+    cat_collar_id serial PRIMARY KEY,    
+    name text NOT NULL UNIQUE,    
     color_id integer NOT NULL,
-	
+    
     FOREIGN KEY (color_id) references ctb.color(color_id)
 );
 
@@ -113,11 +112,11 @@ comment on column ctb.cat_collar.color_id is 'ID цвета';
 
 -- cat_house_space table
 create table ctb.cat_house_space (
-    cat_house_space_id serial PRIMARY KEY,	
+    cat_house_space_id serial PRIMARY KEY,    
     name text NOT NULL UNIQUE,
-	short_name varchar(10),
+    short_name varchar(10),
     color_id integer NOT NULL,
-	
+    
     FOREIGN KEY (color_id) references ctb.color(color_id)
 );
 
@@ -129,10 +128,10 @@ comment on column ctb.cat_house_space.color_id is 'ID цвета';
 
 -- cat_tag table
 create table ctb.cat_tag (
-    cat_tag_id serial PRIMARY KEY,	
-    name text NOT NULL UNIQUE,	
+    cat_tag_id serial PRIMARY KEY,    
+    name text NOT NULL UNIQUE,    
     color_id integer,
-	
+    
     FOREIGN KEY (color_id) references ctb.color(color_id)
 );
 
@@ -143,13 +142,13 @@ comment on column ctb.cat_tag.color_id is 'ID цвета';
 
 -- cat2cat_tag table
 create table ctb.cat2cat_tag (
-    cat2cat_tag_id serial PRIMARY KEY,	
+    cat2cat_tag_id serial PRIMARY KEY,    
     cat_id integer NOT NULL,
-	cat_tag_id integer NOT NULL,
-	
+    cat_tag_id integer NOT NULL,
+    
     FOREIGN KEY (cat_id) references ctb.cat(cat_id),
-	FOREIGN KEY (cat_tag_id) references ctb.cat_tag(cat_tag_id),
-	UNIQUE (cat_id, cat_tag_id)
+    FOREIGN KEY (cat_tag_id) references ctb.cat_tag(cat_tag_id),
+    UNIQUE (cat_id, cat_tag_id)
 );
 
 comment on table ctb.cat2cat_tag is 'словарь для связи кошек и тегов';
@@ -159,13 +158,13 @@ comment on column ctb.cat2cat_tag.cat_tag_id is 'ID тега';
 
 --volunteer table 
 create table ctb.volunteer (
-	volunteer_id serial PRIMARY KEY,
-	notion_volunteer_id text UNIQUE,
-	name text NOT NULL,
-	notion_user_id integer UNIQUE,
-	telegram_account text,
-	address text,
-	location text
+    volunteer_id serial PRIMARY KEY,
+    notion_volunteer_id text UNIQUE,
+    name text NOT NULL,
+    notion_user_id integer UNIQUE,
+    telegram_account text,
+    address text,
+    location text
 );
 
 comment on table ctb.volunteer is 'справочник волонтёров';
@@ -179,14 +178,14 @@ comment on column ctb.volunteer.location is 'координаты в приго�
 
 -- additional constraints to main table - ctb.cat
 alter table ctb.cat 
-	add foreign key (cat_sex_id) 
-	references ctb.cat_sex(cat_sex_id);
+    add foreign key (cat_sex_id) 
+    references ctb.cat_sex(cat_sex_id);
 alter table ctb.cat 
-	add foreign key (responsible_volunteer_id) 
-	references ctb.volunteer(volunteer_id);
+    add foreign key (responsible_volunteer_id) 
+    references ctb.volunteer(volunteer_id);
 alter table ctb.cat 
-	add foreign key (cat_collar_id) 
-	references ctb.cat_collar(cat_collar_id);
+    add foreign key (cat_collar_id) 
+    references ctb.cat_collar(cat_collar_id);
 alter table ctb.cat 
-	add foreign key (cat_house_space_id) 
-	references ctb.cat_house_space(cat_house_space_id);
+    add foreign key (cat_house_space_id) 
+    references ctb.cat_house_space(cat_house_space_id);
