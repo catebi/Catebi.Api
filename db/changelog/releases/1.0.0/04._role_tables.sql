ec62a4cbe34765de6518280a44128c53f5bfc685
tk0nik <konpedalnyitanya@gmail.com>
2023-11-01 06:16:44 +0400

    Исправлены баги

--- db/changelog/releases/1.0.0/04._role_tables.sql
+++ db/changelog/releases/1.0.0/04._role_tables.sql
@@ -11,7 +11,7 @@ create table ctb.role (
 
 comment on table ctb.role is 'Список ролей волонтёров';
 comment on column ctb.role.role_id is 'ID роли';
-comment on column ctb.role.nameis 'Наименование роли';
+comment on column ctb.role.name is 'Наименование роли';
 
 -- ctb.permission table
 create table ctb.permission (
@@ -21,18 +21,18 @@ create table ctb.permission (
 
 comment on table ctb.permission is 'Список прав (разрешений/доступов)';
 comment on column ctb.permission.permission_id is 'ID права';
-comment on column ctb.permission.nameis 'Наименование права ';
+comment on column ctb.permission.name is 'Наименование права';
 
 -- ctb.role_permission table
 create table ctb.role_permission (
 	role_permission_id serial PRIMARY KEY,
     role_id integer NOT NULL,
     permission_id integer NOT NULL,
-	unique (role_id, prtmission_id)
+	unique (role_id, permission_id)
 );
 
 comment on table ctb.role_permission is 'Таблица связи роли с разрешениями';
-comment on column role_permission_id is 'ID соотношения';
+comment on column ctb.role_permission.role_permission_id is 'ID соотношения';
 comment on column ctb.role_permission.role_id is 'ID роли';
 comment on column ctb.role_permission.permission_id is 'ID разрешения';
 
@@ -41,6 +41,10 @@ create table ctb.volunteer_role (
     role_id integer NOT NULL,
     volunteer_id integer NOT NULL
 );
+
+comment on table ctb.volunteer_role is 'Таблица связи "волонтёр-роль"';
+comment on column ctb.volunteer_role.role_id is 'ID роли';
+comment on column ctb.volunteer_role.volunteer_id is 'ID волонтёра';
 
 -- foreign keys 
 -- FK in role_permission 
