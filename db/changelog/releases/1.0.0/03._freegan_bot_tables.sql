7dd071e8a8632466dba1facd0e09ad38049af99b
tk0nik <konpedalnyitanya@gmail.com>
2023-12-09 22:13:13 +0400

    Фикс кодировки

--- db/changelog/releases/1.0.0/03._freegan_bot_tables.sql
+++ db/changelog/releases/1.0.0/03._freegan_bot_tables.sql
@@ -11,18 +11,18 @@ create table freegan.chat(
 	chat_url text unique not null
 );
 
-comment on table freegan.chat is 'Перечень ссылок на telegram-чаты, на которые подписывается бот';
-comment on column freegan.chat.chat_id is 'ID чата';
-comment on column freegan.chat.chat_url is 'URL чата';
+comment on table freegan.chat is 'РџРµСЂРµС‡РµРЅСЊ СЃСЃС‹Р»РѕРє РЅР° telegram-С‡Р°С‚С‹, РЅР° РєРѕС‚РѕСЂС‹Рµ РїРѕРґРїРёСЃС‹РІР°РµС‚СЃСЏ Р±РѕС‚';
+comment on column freegan.chat.chat_id is 'ID С‡Р°С‚Р°';
+comment on column freegan.chat.chat_url is 'URL С‡Р°С‚Р°';
 
 create table freegan.keyword(
 	keyword_id serial PRIMARY KEY,
 	keyword_text text unique not null
 );
 
-comment on table freegan.keyword is 'Ключевые слова, по которым бот производит поиск';
-comment on column freegan.keyword.keyword_id is 'ID слова';
-comment on column freegan.keyword.keyword_text is 'Слово';
+comment on table freegan.keyword is 'РљР»СЋС‡РµРІС‹Рµ СЃР»РѕРІР°, РїРѕ РєРѕС‚РѕСЂС‹Рј Р±РѕС‚ РїСЂРѕРёР·РІРѕРґРёС‚ РїРѕРёСЃРє';
+comment on column freegan.keyword.keyword_id is 'ID СЃР»РѕРІР°';
+comment on column freegan.keyword.keyword_text is 'РЎР»РѕРІРѕ';
 
 create table freegan.chat_keyword(
 	chat_keyword_id serial PRIMARY KEY,
@@ -32,19 +32,19 @@ create table freegan.chat_keyword(
 	unique (chat_id, keyword_id)
 );
 
-comment on table freegan.chat_keyword is 'Таблица для связи чатов и ключевых слов';
-comment on column freegan.chat_keyword.chat_keyword_id is 'ID связки';
-comment on column freegan.chat_keyword.chat_id is 'ID чата';
-comment on column freegan.chat_keyword.keyword_id is 'ID кючевого слова';
+comment on table freegan.chat_keyword is 'РўР°Р±Р»РёС†Р° РґР»СЏ СЃРІСЏР·Рё С‡Р°С‚РѕРІ Рё РєР»СЋС‡РµРІС‹С… СЃР»РѕРІ';
+comment on column freegan.chat_keyword.chat_keyword_id is 'ID СЃРІСЏР·РєРё';
+comment on column freegan.chat_keyword.chat_id is 'ID С‡Р°С‚Р°';
+comment on column freegan.chat_keyword.keyword_id is 'ID РєСЋС‡РµРІРѕРіРѕ СЃР»РѕРІР°';
 
 create table freegan.restriction(
 	restriction_id serial PRIMARY KEY,
 	restriction_text text unique not null
 );
 
-comment on table freegan.restriction is 'Справочник ограничений, присваеваемых ключевым словам';
-comment on column freegan.restriction.restriction_id is 'ID ограничения';
-comment on column freegan.restriction.restriction_text is 'Текст ограничения, например, "Искать по этому слову только в чатах о животных"';
+comment on table freegan.restriction is 'РЎРїСЂР°РІРѕС‡РЅРёРє РѕРіСЂР°РЅРёС‡РµРЅРёР№, РїСЂРёСЃРІР°РµРІР°РµРјС‹С… РєР»СЋС‡РµРІС‹Рј СЃР»РѕРІР°Рј';
+comment on column freegan.restriction.restriction_id is 'ID РѕРіСЂР°РЅРёС‡РµРЅРёСЏ';
+comment on column freegan.restriction.restriction_text is 'РўРµРєСЃС‚ РѕРіСЂР°РЅРёС‡РµРЅРёСЏ, РЅР°РїСЂРёРјРµСЂ, "РСЃРєР°С‚СЊ РїРѕ СЌС‚РѕРјСѓ СЃР»РѕРІСѓ С‚РѕР»СЊРєРѕ РІ С‡Р°С‚Р°С… Рѕ Р¶РёРІРѕС‚РЅС‹С…"';
 
 create table freegan.keyword_restriction(
 	keyword_restriction_id serial PRIMARY KEY,
@@ -54,10 +54,10 @@ create table freegan.keyword_restriction(
 	unique (keyword_id, restriction_id)
 );
 
-comment on table freegan.keyword_restriction is 'Таблица для связи ключевых слов и ограничений';
-comment on column freegan.keyword_restriction.keyword_restriction_id is 'ID связки';
-comment on column freegan.keyword_restriction.keyword_id is 'ID ключевого слова';
-comment on column freegan.keyword_restriction.restriction_id is 'ID ограничения';
+comment on table freegan.keyword_restriction is 'РўР°Р±Р»РёС†Р° РґР»СЏ СЃРІСЏР·Рё РєР»СЋС‡РµРІС‹С… СЃР»РѕРІ Рё РѕРіСЂР°РЅРёС‡РµРЅРёР№';
+comment on column freegan.keyword_restriction.keyword_restriction_id is 'ID СЃРІСЏР·РєРё';
+comment on column freegan.keyword_restriction.keyword_id is 'ID РєР»СЋС‡РµРІРѕРіРѕ СЃР»РѕРІР°';
+comment on column freegan.keyword_restriction.restriction_id is 'ID РѕРіСЂР°РЅРёС‡РµРЅРёСЏ';
 
 -- foreign keys
 --chat_keyword
