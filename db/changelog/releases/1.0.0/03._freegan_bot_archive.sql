69e29245eb542f76804614f68f7049fd2e72e24e
tk0nik <konpedalnyitanya@gmail.com>
2024-01-10 19:45:59 +0400

    Пофикшена хуйня

--- db/changelog/releases/1.0.0/03._freegan_bot_archive.sql
+++ db/changelog/releases/1.0.0/03._freegan_bot_archive.sql
@@ -4,15 +4,15 @@ drop table if exists frgn.message;
 
 create table frgn.message(
     message_id serial primary key,
-	original_text text not null,
-	lemmatized_text text not null,
-	chat_link text not null,
-	accepted bool not null,
+    original_text text not null,
+    lemmatized_text text not null,
+    chat_link text not null,
+    accepted bool not null
 );
 
-comment on table frgn. is 'Архив сообщений, прошедших через бот для разметки';
-comment on column frgn. is 'ID записи';
-comment on column frgn. is 'Исходный текст сообщения';
-comment on column frgn. is 'Текст сообщения после лемматизации';
-comment on column frgn. is 'Ссылка на чат, откуда бот сообщение взял';
-comment on column frgn. is 'Принято ли сообщение по текущему набору правил';
+comment on table frgn.message is 'Архив сообщений, прошедших через бот для разметки';
+comment on column frgn.message.message_id is 'ID записи';
+comment on column frgn.message.original_text is 'Исходный текст сообщения';
+comment on column frgn.message.lemmatized_text is 'Текст сообщения после лемматизации';
+comment on column frgn.message.chat_link is 'Ссылка на чат, откуда бот сообщение взял';
+comment on column frgn.message.accepted is 'Принято ли сообщение по текущему набору правил';
