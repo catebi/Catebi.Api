ab5c8a16b81515aad5e2761c2dd392c06a91587c
ks <zejafo@gmail.com>
2024-03-18 14:26:07 +0400

    #60 fix donation_chat scripts; restructure and rename for db scripts

--- db/changelog/releases/1.0.0/03._freegan_bot_tables.sql
+++ db/changelog/releases/1.0.0/03._freegan_bot_tables.sql
@@ -1,5 +1,6 @@
 drop table if exists frgn.message;
 drop table if exists frgn.donation_message_reaction;
+drop table if exists frgn.donation_chat;
 
 -- freegan_bot_archive
 
@@ -33,3 +34,22 @@ comment on column frgn.donation_message_reaction.message_id is 'ID сообще�
 comment on column frgn.donation_message_reaction.content is 'Текст сообщения';
 comment on column frgn.donation_message_reaction.like_count is 'Количество реакций 👍';
 comment on column frgn.donation_message_reaction.dislike_count is 'Количество реакций 👎';
+
+-- freegan_donation_chat
+create table frgn.donation_chat(
+    donation_chat_id serial primary key,
+    chat_url text not null,
+    is_actual bool not null,
+    is_connected bool not null,
+    created_date timestamp default timezone('utc', now())
+);
+
+comment on table frgn.donation_chat is 'Чаты барахолок для фригана';
+comment on column frgn.donation_chat.donation_chat_id is 'id';
+comment on column frgn.donation_chat.chat_url is 'Ссылка на чат';
+comment on column frgn.donation_chat.is_actual is 'Признак актуальности';
+comment on column frgn.donation_chat.is_connected is 'Признак подключения Мисс Марпл к чату';
+comment on column frgn.donation_chat.created_date is 'Дата создания барахолки';
+
+-- constraints
+alter table frgn.donation_chat add constraint unq_donation_chat_chat_url unique(chat_url);
