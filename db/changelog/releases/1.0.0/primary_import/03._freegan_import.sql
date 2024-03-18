ad96f6a131284e7efda42fa653f2cce2023bb817
ks <zejafo@gmail.com>
2024-03-18 14:29:15 +0400

    #60 remove sensitive data

--- db/changelog/releases/1.0.0/primary_import/03._freegan_import.sql
+++ db/changelog/releases/1.0.0/primary_import/03._freegan_import.sql
@@ -1,19 +1,4 @@
 INSERT INTO frgn.donation_chat(chat_url, is_actual, is_connected)
 VALUES
-  ('https://t.me/baraholka_tbi', true, true),
-  ('https://t.me/avito_baraholka_tbilisi', true, true),
-  ('https://t.me/baraholka_tbilisi', true, true),
-  ('https://t.me/otdam_tbilisi', true, true),
-  ('https://t.me/tbilisi_obyavleniya', true, true),
-  ('https://t.me/Tbilisi_help', true, true),
-  ('https://t.me/freegantbilisi', true, true),
-  ('https://t.me/Tbilisi_market_bg', true, true),
-  ('https://t.me/rabbitsbaraholka', true, true),
-  ('https://t.me/tbilisi_360', true, true),
-  ('https://t.me/tbilisio', true, true),
-  ('https://t.me/gryzia_chat_ads', true, true),
-  ('https://t.me/tbilisi_baraxolka', true, true),
-  ('https://t.me/tbilisiw', true, true),
-  ('https://t.me/tbilisi_avito', true, true),
-  ('https://t.me/Georgia_Avito', true, true),
-  ('https://t.me/barter_ge', true, true);
+  ('https://t.me/catebitest01', true, true),
+  ('https://t.me/catebitest02', true, true);
