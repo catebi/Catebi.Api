23358c5187146eeee60f6f6a3bb74ff12023c724
tk0nik <konpedalnyitanya@gmail.com>
2024-03-29 20:03:10 +0400

    encoding fixed

--- db/changelog/releases/1.0.0/06._cat_views.sql
+++ db/changelog/releases/1.0.0/06._cat_views.sql
@@ -1,28 +1,28 @@
 drop view if exists ctb.cat_statistics_output;
 
 create view ctb.cat_statistics_output (label, value) as 
-    select 'Кошек находится в КК', count(cat_id)
+    select 'РљРѕС€РµРє РЅР°С…РѕРґРёС‚СЃСЏ РІ РљРљ', count(cat_id)
     from ctb.cat
     where in_date is not null 
         and out_date is null
 
     union    
 
-    select 'Стерилизовано кошек за весь период', count(cat_id)
+    select 'РЎС‚РµСЂРёР»РёР·РѕРІР°РЅРѕ РєРѕС€РµРє Р·Р° РІРµСЃСЊ РїРµСЂРёРѕРґ', count(cat_id)
     from ctb.cat
     where neutered_date is not null
 
     union
 
-    select 'Стерелизовано кошек в текущем месяце', count(cat_id)
+    select 'РЎС‚РµСЂРµР»РёР·РѕРІР°РЅРѕ РєРѕС€РµРє РІ С‚РµРєСѓС‰РµРј РјРµСЃСЏС†Рµ', count(cat_id)
     from ctb.cat
     where extract(month from neutered_date) = extract(month from now())
 
     union
 
-    select 'Стерелизовано кошек за предыдущий месяц', count(cat_id)
+    select 'РЎС‚РµСЂРµР»РёР·РѕРІР°РЅРѕ РєРѕС€РµРє Р·Р° РїСЂРµРґС‹РґСѓС‰РёР№ РјРµСЃСЏС†', count(cat_id)
     from ctb.cat
     where extract(month from neutered_date) = extract(month from now())-1
 ;
 
-comment on view ctb.cat_statistics_output is 'Данные для ежемесечных отчётов об объёмах стерилизации кошек'
+comment on view ctb.cat_statistics_output is 'Р”Р°РЅРЅС‹Рµ РґР»СЏ РµР¶РµРјРµСЃРµС‡РЅС‹С… РѕС‚С‡С‘С‚РѕРІ РѕР± РѕР±СЉС‘РјР°С… СЃС‚РµСЂРёР»РёР·Р°С†РёРё РєРѕС€РµРє'
