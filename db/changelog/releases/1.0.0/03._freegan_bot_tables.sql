9aff06742183939a6f84e11b20fe3205f2cbbf77
tk0nik <konpedalnyitanya@gmail.com>
2024-04-28 18:54:57 +0400

    added: groups of keywords tables

--- db/changelog/releases/1.0.0/03._freegan_bot_tables.sql
+++ db/changelog/releases/1.0.0/03._freegan_bot_tables.sql
@@ -1,7 +1,11 @@
 drop table if exists frgn.message;
 drop table if exists frgn.donation_message_reaction;
 drop table if exists frgn.donation_chat;
-drop table if exists frgn.donation_chat;
+drop table if exists frgn.group_included_keywords;
+drop table if exists frgn.group_excluded_keywords;
+drop table if exists frgn.keywords;
+drop table if exists frgn.groups;
+
 
 -- freegan_bot_archive
 
@@ -52,5 +56,58 @@ comment on column frgn.donation_chat.is_actual is 'Признак актуаль
 comment on column frgn.donation_chat.is_connected is 'Признак подключения Мисс Марпл к чату';
 comment on column frgn.donation_chat.created_date is 'Дата создания барахолки';
 
+
+-- Groups table
+create table frgn.groups (
+    group_id serial primary key,
+    name text not null
+);
+
+comment on table frgn.groups is 'Таблица групп слов';
+comment on column frgn.groups.group_id is 'ID группы';
+comment on column frgn.groups.name is 'Название группы';
+
+-- Keywords table
+create table frgn.keywords (
+    keyword_id serial primary key,
+    group_id int not null,
+    keyword text not null
+);
+
+comment on table frgn.keywords is 'Таблица ключевых слов';
+comment on column frgn.keywords.keyword_id is 'ID ключевого слова';
+comment on column frgn.keywords.group_id is 'ID группы, к которой относится ключевое слово';
+comment on column frgn.keywords.keyword is 'Ключевое слово';
+
+-- Group Included Keywords table
+create table frgn.group_included_keywords (
+    included_keyword_id serial primary key,
+    group_id int not null,
+    keyword text not null
+);
+
+comment on table frgn.group_included_keywords is 'Таблица включенных ключевых слов для группы';
+comment on column frgn.group_included_keywords.included_keyword_id is 'ID включенного ключевого слова';
+comment on column frgn.group_included_keywords.group_id is 'ID группы';
+comment on column frgn.group_included_keywords.keyword is 'Включенное ключевое слово';
+
+-- Group Excluded Keywords table
+create table frgn.group_excluded_keywords (
+    excluded_keyword_id serial primary key,
+    group_id int not null,
+    keyword text not null
+);
+
+comment on table frgn.group_excluded_keywords is 'Таблица исключенных ключевых слов для группы';
+comment on column frgn.group_excluded_keywords.excluded_keyword_id is 'ID исключенного ключевого слова';
+comment on column frgn.group_excluded_keywords.group_id is 'ID группы';
+comment on column frgn.group_excluded_keywords.keyword is 'Исключенное ключевое слово';
+
 -- constraints
 alter table frgn.donation_chat add constraint unq_donation_chat_chat_url unique(chat_url);
+alter table frgn.keywords 
+    add foreign key (group_id) references frgn.groups(group_id);
+alter table frgn.group_included_keywords 
+    add foreign key (group_id) references frgn.groups(group_id);
+alter table frgn.group_excluded_keywords 
+    add foreign key (group_id) references frgn.groups(group_id);
